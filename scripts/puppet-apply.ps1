b1e7ab0303517f3549d4ac93c64e20d35c2bf98f
Ian Ellis <ellisia@me.com>
2020-06-08 23:01:26 +0100

    upgrade to latest puppet (6)

---
+++ scripts/puppet-apply.ps1
@@ -0,0 +1,30 @@
+Expand-Archive c:\windows\temp\puppet.zip c:\ProgramData\PuppetLabs\code\environments\production
+
+set FACTERLIB=c:\programdata\puppetlabs\code\environments\production\facter
+
+& "c:\program files\puppet labs\puppet\bin\puppet" apply c:\ProgramData\PuppetLabs\code\environments\production\manifests\choco.pp
+$resp=$LASTEXITCODE
+if ($resp -eq 0) {
+   echo Success
+} else {
+   echo Failure Reason Given is $resp
+   exit $resp
+}
+
+& "c:\program files\puppet labs\puppet\bin\puppet" apply c:\ProgramData\PuppetLabs\code\environments\production\manifests\choco-config.pp
+$resp=$LASTEXITCODE
+if ($resp -eq 0) {
+   echo Success
+} else {
+   echo Failure Reason Given is $resp
+   exit $resp
+}
+
+& "c:\program files\puppet labs\puppet\bin\puppet" apply c:\ProgramData\PuppetLabs\code\environments\production\manifests\site.pp
+$resp=$LASTEXITCODE
+if ($resp -eq 0) {
+   echo Success
+} else {
+   echo Failure Reason Given is $resp
+   exit $resp
+}
--- scripts/puppet-download.cmd
+++ scripts/puppet-download.cmd
@@ -1,1 +1,1 @@
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi', 'C:\Windows\Temp\puppet.msi')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://downloads.puppetlabs.com/windows/puppet6/puppet-agent-x64-latest.msi', 'C:\Windows\Temp\puppet.msi')"
